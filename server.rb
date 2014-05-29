6ba4d8b390b62cc6949606ea3293fbb05c53fb3e
Sam Gladstone <gladston@umich.edu>
2014-05-29 15:24:44 -0400

    Core work requirements met

--- server.rb
+++ server.rb
@@ -1,22 +1,39 @@
 require 'sinatra'
+# require 'sinatra/contrib/all'
 require 'pg'
 require 'pry'
 require './helper_methods.rb'
 
 def get_articles
   connection = PG.connect(dbname: 'slacker_articles')
-  articles = connection.exec('SELECT * FROM articles
-                                ORDER BY articles.created_at DESC')
+  articles = connection.exec('SELECT * FROM articles ORDER BY articles.created_at DESC')
   connection.close
 
   articles
 end
 
+def post_article(title, url, description)
+  sql = 'INSERT INTO articles (title, url, description, created_at) VALUES ($1, $2, $3, NOW())'
+
+  connection = PG.connect(dbname: 'slacker_articles')
+  connection.exec_params(sql, [title, url, description])
+  connection.close
+end
+
 get '/' do
+  redirect '/articles'
+end
+
+get '/articles' do
   @articles = get_articles
   erb :index
 end
 
 get '/articles/new' do
-  # TODO form page
+  erb :form_page
+end
+
+post '/articles/new' do
+  post_article(params["title"], params["url"], params["description"])
+  redirect '/articles'
 end
